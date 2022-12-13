30aeb9e9647d997c5f63f9b7511373422469c870
jaysunl <74435747+jaysunl@users.noreply.github.com>
2022-12-12 19:52:11 -0800

    finish all problems

--- problems.java
+++ problems.java
@@ -1,18 +1,154 @@
 import java.util.*;
+import java.io.*;
 
 public class problems {
-    public static void main(String[] args) {
-        try {
-            File myObj = new File("filename.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-              String data = myReader.nextLine();
-              System.out.println(data);
+
+    static Comparator<Integer[]> LampComparator = Comparator.comparing(i -> i[1]);
+
+    public static long minCost(int H, List<Integer[]> L /* int[][] list */) {
+        /**
+         * for j = 0, . . . , n
+         * (2) M[0, j] = 0
+         * (3) for i = 1, . . . , n
+         * (4) M[i, 0] = ∞
+         * (5) for i = 1, . . . , n
+         * (6) for j = 1, . . . , n
+         * (7) if j − A[j] ≤ i ≤ j + A[j]:
+         * (8) rb = max(j − A[j] − 1, 0)
+         * (9) M[i, j] = min(M[i, j − 1], A[j] + M[rb, j − 1])
+         * (10) else M[i, j] = M[i, j − 1]
+         * (11) return M[n, n]
+         */
+        // i = houses, j = lamps
+        // L.add(new Integer[] { 0, 0, 0 });
+        L.sort(LampComparator);
+        // Collections.reverse(L);
+        for (Integer[] integers : L) {
+            System.out.println(Arrays.asList(integers));
+        }
+
+        long[][] dp = new long[L.size() + 1][H + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = (long) 0;
+        }
+        for (int j = 1; j < dp[0].length; j++) {
+            dp[0][j] = (long) Integer.MAX_VALUE;
+
+        }
+
+        for (int i = 1; i < dp.length; i++) { // lamps
+            for (int j = 1; j < dp[0].length; j++) { // houses
+
+                if (L.get(i - 1)[1] < j) {
+                    dp[i][j] = (long) Integer.MAX_VALUE;
+                } else if (L.get(i - 1)[0] > j) {
+                    dp[i][j] = dp[i - 1][j];
+
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], L.get(i - 1)[2] + dp[i - 1][(L.get(i - 1)[0]) - 1]);
+                }
+
+                // System.out.print(dp[i - 1][j]);
+                // System.out.print(" ");
+                // System.out.println(L.get(i - 1)[2] + dp[i - 1][L.get(i - 1)[0] - 1]);
+
+                // System.out.print(dp[i][j] + " ");
+                // System.out.print(String.join(" ", String.valueOf(i), " ", String.valueOf(j),
+                // " "));
             }
-            myReader.close();
-          } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-          }
+            // System.out.println("");
+        }
+
+        // for (int i = 0; i < dp.length; i++) {
+        // System.out.println("");
+        // for (int j = 0; j < dp[0].length; j++) {
+        // System.out.print(dp[i][j] + " ");
+        // }
+        // }
+        return dp[dp.length - 1][dp[0].length - 1];
     }
+
+    public static void main(String[] args) throws Exception {
+        /*
+         * char[] C = {'N', 'H', 'N', 'M', 'H', 'M', 'H', 'H', 'H', 'H', 'H', 'H', 'N'};
+         * long val = findMin(C);
+         */
+        int H = 5;
+        List<Integer[]> L = new ArrayList<>();
+        L.add(new Integer[] { 1, 3, 2 });
+        L.add(new Integer[] { 2, 4, 4 });
+        L.add(new Integer[] { 2, 5, 8 });
+        L.add(new Integer[] { 4, 5, 3 });
+        long val = minCost(H, L);
+        System.out.println(val);
+
+        List<Integer[]> myList = new ArrayList<>();
+
+        // for (int j = 0; j <= 2000; j++) {
+        // myList.add(new Integer[3]);
+        // }
+
+        // C:\\Users\\Dice\\IdeaProjects\\tests\\streetlamps.csv
+        try {
+            // Scanner sc = new Scanner(new
+            // File("C:\\Users\\Dice\\IdeaProjects\\tests\\streetlamps.csv"));
+
+            // sc.useDelimiter("\\n|,"); // sets the delimiter pattern
+            // int count = 0;
+            // int i = 0;
+            // while (sc.hasNext()) // returns a boolean value
+            // {
+            // myList.add(new Integer[3]);
+            // if (count == 3) {
+            // count = 0;
+            // i++;
+            // // sc.nextLine();
+            // // System.out.println("");
+            // }
+
+            // myList.set(count,Integer.parseInt(sc.next().replaceAll("[^0-9]+", ""));
+            // // System.out.print(myList.get(i)[count]);
+
+            // count++;
+            // // System.out.print(sc.next()); // find and returns the next complete token
+            // // from
+            // // this scanner
+            // }
+            // // long min = minCost(2000, myList);
+            // // System.out.println(min);
+            // sc.close(); // closes the scanner
+
+            String line = "";
+            String splitBy = ",";
+
+            BufferedReader br = new BufferedReader(new FileReader("streetlamps_82.csv"));
+            while ((line = br.readLine()) != null) // returns a Boolean value
+            {
+                String[] lamp = line.split(splitBy);
+                Integer[] intArray = new Integer[3];
+                for (int i = 0; i < lamp.length; i++) {
+                    intArray[i] = Integer.parseInt(lamp[i]);
+                }
+
+                myList.add(intArray);
+            }
+
+            // for (Integer[] integers : myList) {
+            // System.out.println(Arrays.asList(integers));
+            // }
+
+            // System.out.println(myList.size());
+
+            int houses = 2000;
+            long value = minCost(houses, myList);
+            System.out.println(value);
+
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- problems.py
+++ problems.py
@@ -2,72 +2,228 @@
 import csv
 import math
 
+# PROBLEM 1
+                
+class Graph:
+  def __init__(self, edge_list, num_nodes):
+    self.graph = edge_list
+    self.rev_graph = self.reverse_graph()
+    self.num_nodes = num_nodes
+
+    self.traversed_nodes_p1 = []
+    self.traversed_nodes_p2 = []
+
+    self.counter = 0
+    self.scc_size_list = []
+
+    self.scc_sl()
+
+  def reverse_graph(self):
+    r_graph = [];
+    for e in self.graph:
+      tail = e[0]; 
+      head = e[1];
+      r_graph.append([head, tail])
+    r_graph = sorted(r_graph, key = lambda x: x[0])
+    return r_graph
+
+  def dfs_p1(self, starting_node, g, t_nodes): 
+    if (starting_node not in t_nodes):
+      t_nodes.insert(0, starting_node)
+    for i in range (len(g)): 
+      if (g[i][0] == starting_node and g[i][1] not in t_nodes):
+        self.dfs_p1(g[i][1], g, t_nodes)
+  
+  def dfs_loop_p1 (self):
+    for i in range (1, self.num_nodes+1):
+      self.dfs_p1(i, self.rev_graph, self.traversed_nodes_p1)
+
+  def dfs_p2(self, starting_node, g, t_nodes): 
+    if (starting_node not in t_nodes):
+      self.counter += 1
+      t_nodes.append(starting_node)
+    for i in range (len(g)): 
+      if (g[i][0] == starting_node and g[i][1] not in t_nodes):
+        self.dfs_p2(g[i][1], g, t_nodes)
+  
+  def dfs_loop_p2 (self):
+    for i in self.traversed_nodes_p1:
+      self.counter = 0
+      self.dfs_p2(i, self.graph, self.traversed_nodes_p2)
+      if self.counter > 0:
+        self.scc_size_list.append(self.counter)
+  
+  def scc_sl (self):
+    self.dfs_loop_p1()
+    self.dfs_loop_p2()
+
+    self.scc_size_list.sort()
+    self.scc_size_list.reverse()
+
+# Create a graph given in the above diagram
+edges = []
+with open("color_07.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    count = 0
+    for row in csvreader:
+        if row[0] == "vertex":
+            continue
+        else:
+            if len(row) == 1:
+                continue
+            else:
+                # print("row", row)
+                for i in range(len(row)):
+                    if i != 0 and row[i] != '':
+                        edge = (int(row[0]), int(row[i]))
+                        edges.append(edge)
+            count += 1
+list = Graph(edges, count)
+print(list.scc_size_list)
+
 # PROBLEM 2
-# adjMatrix = []
-# with open("numpaths_82.csv", 'r') as file:
-#     csvreader = csv.reader(file)
-#     vertex = 0
-#     for row in csvreader:
-#         list = row.split(',')
-#         for v in list:
-#             if v == "blank":
-#                 adjMatrix[vertex][v] = None
-#             else:
-#                 adjMatrix[vertex][v] = v
+
+
+# Python program for Dijkstra's single
+# source shortest path algorithm. The program is
+# for adjacency matrix representation of the graph
+ 
+# Library for INT_MAX
+import sys
+ 
+ 
+class Graph():
+ 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+ 
+    def printSolution(self, minPaths):
+        print("Min Paths")
+        for node in range(self.V):
+            print(node, "\t", minPaths[node])
+ 
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
+ 
+        # Initialize minimum distance for next node
+        min = sys.maxsize
+ 
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for u in range(self.V):
+            if dist[u] < min and sptSet[u] == False:
+                min = dist[u]
+                min_index = u
+ 
+        return min_index
+ 
+    # Function that implements Dijkstra's single source
+    # shortest path algorithm for a graph represented
+    # using adjacency matrix representation
+    def dijkstra(self, src):
+        
+        minPaths = [0] * self.V
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        minPaths[src] = 1
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+ 
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # x is always equal to src in first iteration
+            x = self.minDistance(dist, sptSet)
+ 
+            # Put the minimum distance vertex in the
+            # shortest path tree
+            sptSet[x] = True
+ 
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for y in range(self.V):
+                if self.graph[x][y] > 0 and sptSet[y] == False and dist[y] > dist[x] + self.graph[x][y]:
+                    dist[y] = dist[x] + self.graph[x][y]
+                    minPaths[y] = minPaths[x]
+                elif self.graph[x][y] > 0 and sptSet[y] == False and dist[y] == dist[x] + self.graph[x][y]:
+                    minPaths[y] += minPaths[x]            
+        self.printSolution(minPaths)
+adjMatrix = []
+with open("numpaths_07.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    vertex = 0
+    for row in csvreader:
+        list = []
+        for i in range(len(row)):
+            if row[i] == "blank":
+                list.append(0)
+            else:
+                list.append(int(row[i]))
+        adjMatrix.append(list)
+g = Graph(100)
+g.graph = adjMatrix
+g.dijkstra(0)
 
 # PROBLEM 3
-# socks = []
-# threshold = 0.25
-# pairs = 0
-# with open("socks_82.csv", 'r') as file:
-#     csvreader = csv.reader(file)
-#     for row in csvreader:
-#         try:
-#             socks.append(float(row[0]))
-#         except ValueError:
-#             continue
+socks = []
+threshold = 0.25
+pairs = 0
+with open("socks_07.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        try:
+            socks.append(float(row[0]))
+        except ValueError:
+            continue
  
-# socks.sort()
-# print(socks)
-# i = 0
-# while i < len(socks)-1:
-#     if abs(socks[i] - socks[i+1]) <= threshold:
-#         print("pair", socks[i], socks[i+1])
-#         pairs = pairs + 1
-#         i = i + 2
-#         continue
-#     elif abs(socks[i] - socks[i+1]) > threshold:
-#         i = i + 1
-#         continue
+socks.sort()
+print(socks)
+i = 0
+while i < len(socks)-1:
+    if abs(socks[i] - socks[i+1]) <= threshold:
+        print("pair", socks[i], socks[i+1])
+        pairs = pairs + 1
+        i = i + 2
+        continue
+    elif abs(socks[i] - socks[i+1]) > threshold:
+        i = i + 1
+        continue
 
-# print(pairs)
+print(pairs)
 
 # PROBLEM 4
 
-lamps = []
-with open("streetlamps.csv", 'r') as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        if row[0] == "s_i":
-            continue
-        else:
-            lamp = (int(row[0]), int(row[1]), int(row[2]))
-            lamps.append(lamp)
+# lamps = []
+# with open("example.csv", 'r') as file:
+#     csvreader = csv.reader(file)
+#     for row in csvreader:
+#         if row[0] == "s_i":
+#             continue
+#         else:
+#             lamp = (int(row[0]), int(row[1]), int(row[2]))
+#             lamps.append(lamp)
+# dp = []
+# for i in range(len(lamps) + 1):
+#     row = []
+#     for j in range(10 + 1):
+#         if i == 0:
+#             row.append(2**31)
+#         else:
+#             row.append(0)
+#     dp.append(row)
 
-l = [[0] * 2000] * (len(lamps))
-for i in range(len(lamps)):
-    l[i][0] = 0
-for j in range(2000):
-    l[0][j] = 2**31
-
-print(l[5][0])
-
-for i in range(1, len(lamps)-1):
-    for j in range(1, 1999):
-        if lamps[i][1] < j:
-            l[i][j] = 2**31
-        if lamps[i][0] > j:
-            l[i][j] = l[i-1][j]
-        l[i][j] = min(l[i-1][j], lamps[i][2] + l[i-1][lamps[i][0]-1])
-# sorted(lamps, key=lambda lamp: lamp[2])
-print(l[len(l)-1][len(l[0])-1])
+# for i in range(1, len(dp)):
+#     for j in range(1, len(dp[0])):
+#         if lamps[i-1][1] < j:
+#             dp[i][j] = 2**31
+#         if lamps[i-1][0] > j:
+#             dp[i][j] = dp[i-1][j]
+#         dp[i][j] = min(dp[i-1][j], lamps[i-1][2] + dp[i-1][lamps[i-1][0]-1])
+# print(dp)
+# print(dp[len(dp)-1][len(dp[0])-1])
